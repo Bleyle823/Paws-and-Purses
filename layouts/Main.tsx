df1e3a4d851bcb8e37bf929add7af6deda67fdbf
Bleyle <Bennybleylle09@gmail.com>
2025-01-30 05:36:45 -0800

    Add files via upload

--- layouts/Main.tsx
+++ layouts/Main.tsx
@@ -7,7 +7,7 @@ type LayoutType = {
   children?: React.ReactNode;
 }
 
-export default ({ children, title = 'Next.js Ecommerce' }: LayoutType) => {
+export default ({ children, title = 'Paws and Purses 🐾' }: LayoutType) => {
   const router = useRouter();
   const pathname = router.pathname;
 
